7d7648738f0190819e73158dedf74889a2b1207c
James Kemp <me@jckemp.com>
2013-08-10 20:40:39 +0100

    Updated mwi-plugin links

--- inc/admin.php
+++ inc/admin.php
@@ -3,11 +3,11 @@
 	<div id="mwi_right">
 		<div class="mwi_meta">
 			<h3>Documentation</h3>
-			<p>Visit <a href="http://www.magentowp.com" target="_blank">www.magentowp.com</a> for all the latest documentation; including usage instructions, functions, examples and more.</p>
+			<p>Visit <a href="http://www.mwi-plugin.com" target="_blank">www.mwi-plugin.com</a> for all the latest documentation; including usage instructions, functions, examples and more.</p>
 			<h3>Support</h3>
-			<p>If you require some support, please check out the <a href="http://magentowp.com/documentation/faq/" target="_blank">FAQ section</a> on the MWI website. If that doesn't help, try posting in the <a href="http://wordpress.org/support/plugin/magento-wordpress-integration" target="_blank">support forums</a>. If all else fails, please <a href="mailto:sales@magentowp.com" target="_blank">send an email</a>.</p>
+			<p>If you require some support, please check out the <a href="http://mwi-plugin.com/documentation/faq/" target="_blank">FAQ section</a> on the MWI website. If that doesn't help, try posting in the <a href="http://wordpress.org/support/plugin/magento-wordpress-integration" target="_blank">support forums</a>. If all else fails, please <a href="mailto:sales@mwi-plugin.com" target="_blank">send an email</a>.</p>
 			<h3>Add-ons</h3>
-			<p>To purchase add-ons for MWI, please visit the <a href="http://magentowp.com/add-ons/" target="_blank">add-ons store</a>.</p>
+			<p>To purchase add-ons for MWI, please visit the <a href="http://mwi-plugin.com/add-ons/" target="_blank">add-ons store</a>.</p>
 			<h3>Twitter</h3>
 			<a href="https://twitter.com/jamesckemp" class="twitter-follow-button" data-show-count="false">Follow @jamesckemp</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
